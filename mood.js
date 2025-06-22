29e8a017c11b9b5f78c8e68094b3bff79e304812
SUBHASISH NAYAK <153982823+NayakSubhasish@users.noreply.github.com>
2025-06-22 16:53:21 +0530

    Add files via upload

---
+++ mood.js
@@ -0,0 +1,34 @@
+let photo = document.getElementById('moodImage');
+let sad = document.getElementById('b2');
+let happy = document.getElementById('b1');
+
+happy.addEventListener('click', function () {
+    console.log("hehehe");
+    photo.src = "happy.jpg"; 
+});
+
+sad.addEventListener('click', ()=> {
+    console.log("aww");
+    photo.src = "sad.jpg";  
+});
+
+setInterval(()=>{
+    photo.src= normal.png
+},400);
+
+
+
+
+
+
+function sum(a, b){
+    console.log(a+b);
+}
+sum(9,8);
+const add1=(x,y)=>{
+    console.log(x+y);
+}
+add1(4,5)
+
+
+///  function() || ()=> ..... same functiononing.
